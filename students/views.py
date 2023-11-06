dbaf9f7be65e58e57571d8b63e2d6e8ddb6e88f9
CrazyMonkey0 <hubcio1765@gmail.com>
2023-11-06 11:57:42 +0100

    Getting access to course content

--- students/views.py
+++ students/views.py
@@ -1,8 +1,14 @@
+from typing import Any
+from django.db import models
+from django.db.models.query import QuerySet
 from django.urls import reverse_lazy
 from braces.views import LoginRequiredMixin
 from django.views.generic.edit import CreateView, FormView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from courses.models import Course
 from .forms import CourseEnrollForm
 
 
@@ -30,3 +36,35 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
 
     def get_success_url(self):
         return reverse_lazy('student_course_detail', args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super(StudentCourseListView, self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super(StudentCourseDetailView, self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super(StudentCourseDetailView,
+                        self).get_context_data(**kwargs)
+        # Download course  object
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # Download the current module.
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+        else:
+
+            # Download the frist module
+            context['module'] = course.modules.all()[0]
+        return context
