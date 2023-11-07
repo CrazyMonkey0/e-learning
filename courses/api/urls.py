1f47f25f84b9d72e5fa2c91013dd174da6361106
CrazyMonkey0 <hubcio1765@gmail.com>
2023-11-07 22:08:47 +0100

    Create a collection of views and routers

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
 app_name = 'courses'
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
 
 urlpatterns = [
     path('subjects/',
@@ -16,4 +20,5 @@ urlpatterns = [
          views.CourseEnrollView.as_view(),
          name='course_enroll'),
 
+    path('', include(router.urls)),
 ]
