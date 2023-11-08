95abe5b026dac713e4b98a2ef5cf28df8e5204b0
CrazyMonkey0 <hubcio1765@gmail.com>
2023-11-08 17:22:03 +0100

    Update consumer to break the news

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,18 +1,41 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = 'chat_%s' % self.id
+        # Join to group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         # Connection accepted
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # Leave groupe
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
     # Receiving a message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({'message': message}))
+        # Send message to groupe
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+            }
+        )
+
+    # Receiving messages from the chat room group
+    def chat_message(self, event):
+        # Send messages to WebSocket.
+        self.send(text_data=json.dumps(event))
