c88304977299a38539d9cd0b3712385eaee0a4de
CrazyMonkey0 <hubcio1765@gmail.com>
2023-11-08 11:39:56 +0100

    Implementation of the chat room view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def course_chat_room(request, course_id):
+    try:
+        # Download a course with an ID that matches the ID of the course that the current user has joined.
+        course = request.user.courses_joined.get(id=course_id)
+    except:
+        # The user is not a member of this course or the course does not exist.
+        return HttpResponseForbidden()
+    return render(request, 'chat/room.html', {'course': course})
