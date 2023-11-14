4e2e37db1c9b83f917ea15cc2ac52b5ca51fddd4
CrazyMonkey0 <hubcio1765@gmail.com>
2023-11-14 12:03:33 +0100

    Manage settings for multiple environments

--- edu/settings/base.py
+++ edu/settings/base.py
@@ -15,7 +15,7 @@ import os
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7bq0zi9x&g*4o+&w04yk^y2$35$ch)e-6at*w$pe%prii9w=ww'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'edu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
---
+++ edu/settings/pro.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = (
+    ('Monkey', 'Monkey@mydomain.com'),
+)
+
+ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
+
+DATABASES = {
+    'default': {
+    }
+}
